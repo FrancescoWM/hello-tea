2d948df46442b8756bd7e61d3312322abb6a8756
Francesco Pagnanelli <f.pagnanelli@gmail.com>
2024-02-22 09:45:05 +0100

    Added Dependencies

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // index.js
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Ciao Mondo!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App in ascolto sulla porta ${PORT}`);
+});
 function helloTea(name) {
     return `Hello, ${name}!`;
   }
